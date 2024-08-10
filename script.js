71d25e40e2e503e12b7649877ad05ab80a0dc560
Asan saheb Dherisha <129145379+ASANSAHEBDHERISHA@users.noreply.github.com>
2024-08-10 22:35:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example of adding interactivity
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Netflix homepage loaded');
+    
+    // You can add JavaScript functionalities here
+});
